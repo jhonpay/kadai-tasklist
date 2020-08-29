e3f93d16a7e34d35711d2d5fd085661dd2ecfa13
sakaguchi.youhei <jon777sy@gmail.com>
2020-08-29 12:10:02 +0000

    lesson15 kadai-1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:succes] = "ユーザー登録しました"
+      redirect_to("/")
+    else
+      flash.now[:danger] = "ユーザー登録失敗"
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
